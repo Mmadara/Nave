ca5e4f9f978cc3573dbe03e4ddea95a88ab3ff0a
unknown <williamandres0000@hotmail.com>
2020-04-30 12:00:36 -0500

    mi primer commit

---
+++ Black Dragon/Script.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+
+public class permissionscript : MonoBehaviour
+{
+    GameObject dialog = null;
+    void Start()
+    {
+#if PLATFORM_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+#endif
+    }
+}
+
+
+// here is my device camera script
+
+
+
+
+public class simplecamera : MonoBehaviour
+{
+    static WebCamTexture backCam;
+
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        if (backCam == null)
+        {
+            backCam = new WebCamTexture();
+        }
+        GetComponent<RawImage>().texture = backCam;
+        backCam.Play();
+    }
+}
